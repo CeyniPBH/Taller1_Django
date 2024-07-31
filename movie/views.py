1b9a1b44b472300bb508ad04cdf3e264b78602e7
CeyniPBH <cpbeltranh@eafit.edu.co>
2024-07-30 20:22:06 -0500

    cambio en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Greg Lim'}) #Json
+    return render(request, 'home.html', {'name': 'Ceyni Beltran'}) #Json
