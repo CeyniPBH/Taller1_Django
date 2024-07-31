5eda379c1193cf716a43d830ec7c7844d57187c0
CeyniPBH <cpbeltranh@eafit.edu.co>
2024-07-30 22:18:03 -0500

    busquedaa de películas

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Movie
 # Create your views here.
+
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home page</h1>')
     #return render(request, 'home.html')
     #return render(request, 'home.html', {'name': 'Ceyni Beltran'}) #Json
-    name = 'Ceyni Beltran'
-    return render(request, 'about.html', {'name': name}) #Json
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to Home page</h1>')
