25a353e44ed100d2ebabcd0316f931b01965708c
CeyniPBH <cpbeltranh@eafit.edu.co>
2024-07-30 20:59:31 -0500

    cambio de color en nombre

--- movie/views.py
+++ movie/views.py
@@ -6,4 +6,5 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home page</h1>')
     #return render(request, 'home.html')
     #return render(request, 'home.html', {'name': 'Ceyni Beltran'}) #Json
-    return render(request, 'about.html', {'name': 'Ceyni Beltran'}) #Json
+    name = 'Ceyni Beltran'
+    return render(request, 'about.html', {'name': name}) #Json
